f32e99dc4a1b6cc6ecd9436ed8dd3d662be8d66b
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-26 18:29:35 +0500

    fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -12,30 +11,9 @@ func main() {
 	lambda.Start(handler)
 }
 
-func handler(request interface{}) (interface{}, error) {
-	// Try to unmarshal the request into an APIGatewayProxyRequest
-	if gatewayRequestData, err := json.Marshal(request); err == nil {
-		gatewayRequest := &events.APIGatewayProxyRequest{}
-		if err := json.Unmarshal(gatewayRequestData, gatewayRequest); err == nil {
-			if gatewayRequest.HTTPMethod == "POST" ||
-				gatewayRequest.HTTPMethod == "PUT" ||
-				gatewayRequest.HTTPMethod == "DELETE" ||
-				gatewayRequest.HTTPMethod == "GET" {
-				return handleAPIGatewayEvent(gatewayRequest)
-			}
-		}
-	}
-
-	// Try to unmarshal the request into a CloudWatchEvent
-	if cloudWatchEventData, err := json.Marshal(request); err == nil {
-		cloudWatchEvent := &events.CloudWatchEvent{}
-		if err := json.Unmarshal(cloudWatchEventData, cloudWatchEvent); err == nil {
-			if len(cloudWatchEvent.Detail) > 0 {
-				fmt.Println("Received CloudWatch event with non-empty detail")
-				return handleCloudWatchEvent(cloudWatchEvent)
-			}
-		}
-	}
-
-	return events.APIGatewayProxyResponse{StatusCode: 500}, nil
+func handler(event events.CloudWatchEvent) (string, error) {
+	// Handle CloudWatch Event (event logs) here
+	// Access event.Detail and other properties as needed
+	fmt.Printf(string(event.ID))
+	return string(event.ID), nil
 }
