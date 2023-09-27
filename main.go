00d0568024ad307205a2172ab3b07be6c807a7d8
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-27 16:33:42 +0500

    fix

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func handler(event map[string]interface{}) (string, error) {
 	// Use the eventName in your logic
 	result := fmt.Sprintf("Received custom event: Name=%s", eventName)
 
-	fmt.Printf("Received custom event: Name=%s", eventName)
+	fmt.Printf("Received customize event: Name=%s", eventName)
 
 	return result, nil
 }
