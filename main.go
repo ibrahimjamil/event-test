ada8519458557eea85e66ac25b6293d1f846f7b3
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-27 12:30:51 +0500

    fix

--- main.go
+++ main.go
@@ -11,20 +11,7 @@ func main() {
 }
 
 func handler(event interface{}) (string, error) {
-	// Type assertion to convert event into a struct type
-	customEvent, ok := event.(struct {
-		CustomEventName string `json:"customEventName"`
-	})
+	fmt.Println(event)
 
-	if !ok {
-		return "", fmt.Errorf("Failed to assert event to the expected type")
-	}
-
-	// Access the customEventName field
-	eventName := customEvent.CustomEventName
-
-	// Use the custom event data in your logic
-	result := fmt.Sprintf("Received custom event: Name=%s", eventName)
-
-	return result, nil
+	return "", nil
 }
