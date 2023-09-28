6fd338a651ab7ddae7adfcd06d849705052b7ddb
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-28 10:47:16 +0500

    fix

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/go-stomp/stomp"
 )
 
 func main() {
@@ -18,9 +23,73 @@ func handler(event map[string]interface{}) (string, error) {
 	}
 
 	// Use the eventName in your logic
-	result := fmt.Sprintf("Received custom event: Name=%s", eventName)
+	// result := fmt.Sprintf("Received custom event: Name=%s", eventName)
 
 	fmt.Printf("Received customize event: Name=%s", eventName)
 
-	return result, nil
+	// Get the broker endpoint
+	brokerEndpointIP := os.Getenv("MQ_ENDPOINT_IP")
+	brokerUsername := os.Getenv("BROKER_USERNAME")
+	brokerPassword := os.Getenv("BROKER_PASSWORD")
+	brokerEndpointIP = strings.TrimPrefix(brokerEndpointIP, "stomp+ssl://")
+
+	// Create a tls dial and stomp connect to broker
+	netConn, err := tls.Dial("tcp", brokerEndpointIP, &tls.Config{})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer netConn.Close()
+
+	conn, err := stomp.Connect(netConn,
+		stomp.ConnOpt.Login(brokerUsername, brokerPassword))
+	if err != nil {
+		log.Printf("Failed to connect to the broker: %v", err)
+		return fmt.Sprintf("Failed to connect to the broker: %v", err), err
+	}
+	defer conn.Disconnect()
+
+	fmt.Print("connection established")
+
+	// Send a message to a queue on the broker
+	queueName := "Demo-Queue"
+	message := eventName
+	err = conn.Send(
+		queueName,
+		"text/plain",
+		[]byte(message),
+		nil,
+	)
+	if err != nil {
+		log.Printf("Failed to send message: %v", err)
+		return fmt.Sprintf("Failed to send message: %v", err), err
+	}
+
+	log.Printf("Message sent to the queue: %s", message)
+
+	// Subscribe to a queue on the broker
+	sub, err := conn.Subscribe(queueName, stomp.AckAuto)
+	if err != nil {
+		log.Printf("Failed to subscribe to the queue: %v", err)
+		return fmt.Sprintf("Failed to subscribe to the queue: %v", err), err
+	}
+	defer sub.Unsubscribe()
+
+	fmt.Print("Connection established, waiting for messages...\n")
+
+	// Listen for and process incoming messages
+	var messageBody string
+	for {
+		msg := <-sub.C
+		if msg.Err != nil {
+			log.Printf("Failed to receive message: %v", msg.Err)
+			return fmt.Sprintf("Failed to receive message: %v", msg.Err), msg.Err
+		}
+
+		// Process the received message (you can modify this part as needed)
+		messageBody = string(msg.Body)
+		log.Printf("Received message from the queue: %s", messageBody)
+		break
+	}
+
+	return fmt.Sprintf("Received custom event: Name=%s", eventName), nil
 }
